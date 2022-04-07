49b7e1bf1d5c45e4656f65dd361aea70794242e6
spreitni <70229459+spreitni@users.noreply.github.com>
2022-04-06 19:51:40 -0500

    Add files via upload

---
+++ chatsever/homework 9/Server/ChatWorkerThread.java
@@ -0,0 +1,55 @@
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatWorkerThread extends Thread 
+{
+    private Socket theClientSocket;
+    private PrintStream clientOutput;
+    private Scanner clientInput;
+
+    public ChatWorkerThread(Socket theClientSocket)
+    {
+        try 
+        {
+            System.out.println("Connection Established...");
+            this.theClientSocket = theClientSocket;
+            this.clientOutput = new PrintStream(this.theClientSocket.getOutputStream());    
+            //System.out.println("About to add a printstream");
+            CORE.addClientThreadPrintStream(this.clientOutput);
+
+            this.clientInput = new Scanner(this.theClientSocket.getInputStream());
+        } 
+        catch (Exception e) 
+        {
+            System.err.println("Bad things happened in thread!!!!!");
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void run() 
+    {
+        //this is what the thread does
+
+        this.clientOutput.println("What is your name?");
+        String name = clientInput.nextLine();
+        CORE.broadcastMessage(name + " has joined!");
+        
+        String message;
+        while(true)
+        {
+           
+            message = clientInput.nextLine();
+            if(message.equals("/quit") )
+            {
+                CORE.broadcastMessage(name + "has left the chat");
+                clientOutput.close();
+            }
+            else
+            {
+                CORE.broadcastMessage(message);
+            }
+        }
+    }
+}
